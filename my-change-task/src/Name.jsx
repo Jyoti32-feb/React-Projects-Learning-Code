76c555d51df17896593f676984b6bf97b608708d
Jyoti Shrinivas Pathade <83999132+Jyoti32-feb@users.noreply.github.com>
2026-07-08 09:14:50 +0530

    practising hooks with events

---
+++ my-change-task/src/Name.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+
+
+
+function Name() {
+    const [name, setName] = useState("");
+    const [course, setCourse] = useState("");
+    const changeName = (e) => {
+        setName(e.target.value);
+    }
+    
+    
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+
+      <h3>Name:</h3>
+      <input type="text"
+      value={name}
+      onChange={changeName}
+      />
+
+      <br />
+      <h3>Course:</h3>
+      <input type="text"
+      value={course}
+      onChange={(e) => setCourse(e.target.value)}
+      />
+
+      <br />
+
+     
+
+      <p>My name is {name}</p>
+
+      <p>My course is {course}</p>
+
+      <p>The total length of your name and course is {name.length + course.length}</p>
+    </div>
+  );
+}
+export default Name;
